70eeed2bc8183671f5be02b69e173e927e303229
houimliraed@outlook.fr <houimliraed@outlook.fr>
2024-05-14 05:54:01 +0100

    basic calculations

--- basic_calculations.py
+++ basic_calculations.py
@@ -1,5 +1,13 @@
 # minutes per day
+print('printing how many minutes in a day')
 print(24 * 60 )
+# printing a string
+print('just a string printing')
 print('String')
+# addition
+print("adding two variables")
 print(10+10)
+#substraction
+print('substracting two variables')
 print(2-1)
+print(f"my number is {50} as a string")
