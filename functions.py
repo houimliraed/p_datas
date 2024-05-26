29da8167731164cb72d7aa726bc0941778f5a76d
houimliraed@outlook.fr <houimliraed@outlook.fr>
2024-05-26 02:08:39 +0100

    conditional

--- functions.py
+++ functions.py
@@ -1,8 +1,11 @@
 
 
 def daysToUnits(numberOfDays):
-    return f" Number of hours in days are {60 * numberOfDays}"
-
+    if numberOfDays > 0:
+        return f" Number of hours in days are {60 * numberOfDays}"
+    else:
+        return "you enterd a negative value !"
+    
 userInput = input("input the number of days !\n")
 userInputAsNumber = int(userInput)
 calculatedValue = daysToUnits(userInputAsNumber)
