c84578334aea2030349998060d37ef2258152ae8
houimliraed@outlook.fr <houimliraed@outlook.fr>
2024-05-27 18:22:36 +0100

    while loop

--- functions.py
+++ functions.py
@@ -1,13 +1,30 @@
 
 
+
 def daysToUnits(numberOfDays):
     if numberOfDays > 0:
         return f" Number of hours in days are {60 * numberOfDays}"
     else:
         return "you enterd a negative value !"
     
-userInput = input("input the number of days !\n")
-userInputAsNumber = int(userInput)
-calculatedValue = daysToUnits(userInputAsNumber)
 
-print(calculatedValue)
+def validateAndExcute():
+    try:
+        userInputAsNumber = int(userInput)
+        if userInputAsNumber > 0:
+                calculatedValue = daysToUnits(userInputAsNumber)
+                print(calculatedValue)
+        elif userInputAsNumber == 0:
+                print('check the value it is equal to zero')
+    except ValueError:
+        print('your input is not a valid number !')          
+
+while(True):
+     userInput = input("input the number of days !\n")
+     validateAndExcute()       
+
+
+
+
+    
+
