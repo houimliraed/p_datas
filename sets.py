1e3084e7f23177d9e098335a32f84efb9a3e9d10
houimliraed@outlook.fr <houimliraed@outlook.fr>
2024-05-31 03:22:24 +0100

    sets

---
+++ sets.py
@@ -0,0 +1,9 @@
+mySet = {'10','4','60'}
+# you can only access the element in the set in a loop only
+for element in mySet:
+    print(element)
+# sets has its own built in function , we can use add to add an element to the set
+mySet.add('66')
+print(mySet)
+mySet.remove('10')
+print(mySet)
