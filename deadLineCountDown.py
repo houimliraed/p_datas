60ed080725e8f048f12401692885245218ed2efa
houimliraed@outlook.fr <houimliraed@outlook.fr>
2024-06-10 02:56:08 +0100

    test

--- deadLineCountDown.py
+++ deadLineCountDown.py
@@ -1,9 +1,9 @@
 import datetime
 
-userInput = input('Give me your goal deadline separated by colon:\n')
+"""userInput = input('Give me your goal deadline separated by colon:\n')
 InputList = userInput.split(':') 
 Goal = InputList[0]
 deadline = InputList[1]
 print(InputList)
-datetime.datetime.strptime(deadline, "%d.%m.%Y")
-print(datetime.datetime.strptime(deadline, "%d.%m.%Y"))
+datetime.datetime.strptime(deadline, "%d.%m.%Y")"""
+print(datetime.datetime.today())
