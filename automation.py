46bc53d61678fb623d42bc021e193ee90a192a67
houimliraed@outlook.fr <houimliraed@outlook.fr>
2024-06-19 02:29:31 +0100

    rows

--- automation.py
+++ automation.py
@@ -1,3 +1,13 @@
 import openpyxl
 
 invFile = openpyxl.load_workbook("inventory.xlsx")
+
+productList = invFile['Sheet1']
+
+numberLines = productList.max_row
+
+print(numberLines)
+
+for productRow in range(2,numberLines+1):
+    print(productRow)
+    
---
+++ ex.py
@@ -0,0 +1,4 @@
+myList = [1 , 2 , 3 , "test"]
+for element in myList:
+    print(element)
+    
